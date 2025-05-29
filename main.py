cb5d6e88916b46047de404fb891b65cb20e53fb5
NgocAnhDang <111883222+anhngocdang2723@users.noreply.github.com>
2025-05-29 08:55:17 +0700

    backup

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ app = FastAPI(
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Frontend URL
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["*"],
